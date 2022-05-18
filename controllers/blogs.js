c51af7a8302eb42f0cdfc20f0de573d43aa52bd3
Andres Rivero <andres19rivero@gmail.com>
2022-05-17 20:07:04 -0400

    feat: token authentication

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,13 +4,12 @@ const User = require('../models/user')
 
 blogsRouter.get('/', async (req, res) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
-
   res.json(blogs)
 })
 
 blogsRouter.post('/', async (req, res) => {
   const body = req.body
-  const user = await User.findOne({ username: 'ozy' })
+  const user = await User.find({}).limit(1)
 
   const blog = new Blog({
     title: body.title,
